decc56af0322ef04b709ad206db0568848ce6f58
Jeffrey <jeffreyhu16@gmail.com>
2022-10-07 07:25:04 +0800

    add second planet

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useScene } from 'babylonjs-hook';
-import { StandardMaterial, Texture, MeshBuilder } from '@babylonjs/core';
+import { StandardMaterial, Texture, MeshBuilder, Color3 } from '@babylonjs/core';
 
 function Planets() {
 
@@ -8,11 +8,14 @@ function Planets() {
 
   useEffect(() => {
     if (scene) {
+      const sun = MeshBuilder.CreateSphere('sun', { segments: 16, diameter: 4 }, scene);
+
       const planetMaterial = new StandardMaterial('planetMaterial', scene);
       planetMaterial.diffuseTexture = new Texture('assets/images/sand.png', scene);
-
+      planetMaterial.specularColor = Color3.Black();
 
       const planet = MeshBuilder.CreateSphere('planet1', { segments: 16, diameter: 1 }, scene);
+      planet.position.x = 4;
       planet.material = planetMaterial;
     }
   }, [scene]);
