04619103caef6c8749c362a68851a9768fbfcaf2
Jeffrey <jeffreyhu16@gmail.com>
2022-10-07 09:14:10 +0800

    add multiple planets orbiting

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -20,9 +20,24 @@ function Planets() {
       planetMaterial.diffuseTexture = new Texture('assets/images/sand.png', scene);
       planetMaterial.specularColor = Color3.Black();
 
-      const planet = MeshBuilder.CreateSphere('planet1', { segments: 16, diameter: 1 }, scene);
-      planet.position.x = 4;
-      planet.material = planetMaterial;
+      const speeds = [0.01, -0.01, 0.005];
+      for (let i = 0; i < 3; i++) {
+        const planet = MeshBuilder.CreateSphere(`planet${i}`, { segments: 16, diameter: 1 }, scene);
+        planet.position.x = 2 * i + 4;
+        planet.material = planetMaterial;
+
+        const orbit = {
+          radius: planet.position.x,
+          speed: speeds[i],
+          angle: 0
+        }
+
+        scene.registerBeforeRender(() => {
+          planet.position.x = orbit.radius * Math.sin(orbit.angle);
+          planet.position.z = orbit.radius * Math.cos(orbit.angle);
+          orbit.angle += orbit.speed;
+        });
+      }
     }
   }, [scene]);
 
