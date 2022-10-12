ab7afc0590ee0bf5da1ded8189c33c0b73ff5ae2
Jeffrey <jeffreyhu16@gmail.com>
2022-10-12 12:28:54 +0800

    relocate urls to config file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,19 +4,19 @@ import { MainSpaceState } from '../../schemas';
 import { RoomContext } from '../contexts/roomContext';
 import SceneComponent from './SceneComponent';
 import Backdrop from './Backdrop';
+import config from '../utils/url.json';
 
 function App() {
 
   const [room, setRoom] = useState<Colyseus.Room<MainSpaceState> | null>(null);
 
-  const ENV_URL = 'https://astonishing-stroopwafel-02b744.netlify.app/.netlify/functions/env';
-
   useEffect(() => {
     (async () => {
       try {
-        const res = await fetch(ENV_URL);
+        const res = await fetch(config.ENV_URL);
         const SERVER_URL = await res.json();
         const client = new Colyseus.Client(SERVER_URL);
+
         const room = await client.joinOrCreate<MainSpaceState>('main_space', { name: 'player' });
         room.onStateChange(() => setRoom(room));
       } catch (err) {
