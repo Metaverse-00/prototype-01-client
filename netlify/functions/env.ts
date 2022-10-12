5199fb3504729d34fa5577059969d0534b41fa67
Jeffrey <jeffreyhu16@gmail.com>
2022-10-12 08:48:56 +0800

    refactor testing netlify env variable pt2

--- netlify/functions/env.ts
+++ netlify/functions/env.ts
@@ -3,7 +3,7 @@ import { Handler } from '@netlify/functions';
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: process.env.SERVER_URL
+    body: JSON.stringify(process.env.SERVER_URL)
   }
 }
 
